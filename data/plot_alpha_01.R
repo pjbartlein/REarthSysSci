442d52ca0b5643167c8a64c4b48ec79e05ddceaa
pjbartlein <bartlein@uoregon.edu>
2019-04-06 14:34:41 -0700

    revised .Rmd files

--- data/plot_alpha_01.R
+++ data/plot_alpha_01.R
@@ -6,18 +6,17 @@ library(raster)
 library(rasterVis)
 library(RColorBrewer)
 
-# read a world shapefile
-shp_path <- "/Users/bartlein/Projects/working/geog490/data/shp_files/world2013/"
-shp_name <- "world2013.shp"
+shp_path <- "/Users/bartlein/Dropbox/DataVis/working/geog490/data/shp_files/ne_110m_admin_0_countries/"
+shp_name <- "ne_110m_admin_0_countries.shp"
 shp_file <- paste(shp_path, shp_name, sep="")
 world_shp <- read_sf(shp_file)
 world_outline <- as(st_geometry(world_shp), Class="Spatial")
 
-# plot the shape file
+# plot the outline
 plot(world_outline, col="blue", lwd=1)
 
 # read alpha (AE/PE)
-alpha_path <- "/Users/bartlein/Projects/working/geog490/data/nc_files/"
+alpha_path <- "/Users/bartlein/Dropbox/DataVis/working/geog490/data/nc_files/"
 alpha_name <- "cru10min30_bio.nc"
 alpha_file <- paste(alpha_path, alpha_name, sep="")
 alpha <- raster(alpha_file, varname="mipt")
