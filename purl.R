3a31e890f51d2fef6c3c10c34b72f756d67cd455
pjbartlein <bartlein@uoregon.edu>
2024-01-10 18:13:14 -0800

    update

--- purl.R
+++ purl.R
@@ -1,5 +1,5 @@
 library(knitr)
 
-filename <- "/Users/bartlein/Dropbox/DataVis/working/REarthSysSci/installR" # path to and name of .Rmd file (note: no extension)
+filename <- "/Users/bartlein/Dropbox/DataVis/working/REarthSysSci/usingR" # path to and name of .Rmd file (note: no extension)
 purl(paste(filename, ".Rmd", sep=""), paste(filename, ".R", sep=""), documentation=0)
 
