6f8d53dd793a980f22b47bfd79eb971f5a179e7a
pjbartlein <bartlein@uoregon.edu>
2024-02-10 18:44:59 -0800

    rebuild

--- docs/R/namer.R
+++ docs/R/namer.R
@@ -1,6 +1,6 @@
 library(namer)
 
-filename <- "/Users/bartlein/Dropbox/DataVis/REarthSysSci/maps2.Rmd" # path to and name of .Rmd file 
+filename <- "/Users/bartlein/Dropbox/DataVis/REarthSysSci/anomalies.Rmd" # path to and name of .Rmd file 
 unname_chunks(filename)
 name_chunks(filename)
 
