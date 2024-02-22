eb16c7bf5c2c8534f6666cfe5e3b14bdd4de04e6
pjbartlein <bartlein@uoregon.edu>
2024-02-21 18:07:39 -0800

    Update pat.R

--- pat.R
+++ pat.R
@@ -7,9 +7,5 @@ usethis::gh_token_help()
 usethis::git_sitrep()
 gh::gh_whoami()
 
-
-
-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
-
-MacMini: 
-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
+RStudio 
+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
