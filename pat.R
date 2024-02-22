a73e64cee89e4508845857a3d7119e1bef08337a
pjbartlein <bartlein@uoregon.edu>
2024-02-21 17:44:46 -0800

    updated PAT

---
+++ pat.R
@@ -0,0 +1,15 @@
+library(usethis) # also installs {credentials}
+usethis::create_github_token()
+credentials::set_github_pat() 
+
+# also
+usethis::gh_token_help()
+usethis::git_sitrep()
+gh::gh_whoami()
+
+
+
+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
+
+MacMini: 
+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
